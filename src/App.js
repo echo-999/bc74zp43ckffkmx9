b7e3ff36994601f28e279397139c2dc2ec846bb7
echo-999 <60455353+echo-999@users.noreply.github.com>
2021-09-14 15:26:40 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  const base = 'bc74zp43ckffkmx9';
+  return (
+    <div className="App">
+      Hello Test
+    </div>
+  );
+}
+
+export default App;
